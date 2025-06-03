dc51e832e52d49327ace13f6ab6d6ce314850ff8
Kacper Polak <strixu155@gmail.com>
2025-06-03 13:59:53 +0200

    fix: fix font in hero on small devices

--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -9,11 +9,11 @@ export const Hero = async () => {
 	return (
 		<section className="h-dvh before:block before:h-2/3 before:bg-hero before:bg-cover before:bg-bottom before:shadow-hero-image sm:before:bg-hero-desktop">
 			<div className="-mt-32 flex flex-col items-center gap-6 px-4 sm:-mt-24 sm:px-8 md:gap-8">
-				<h1 className="border-b-3 border-primary px-3 pb-2 text-center font-bebas-neue text-7xl text-white xs:text-8xl sm:px-12 sm:pb-4 lg:text-9xl xl:text-10xl">
+				<h1 className="border-b-3 border-primary px-3 pb-2 text-center font-bebas-neue text-7xl text-white sm:px-12 sm:pb-4 lg:text-9xl xl:text-10xl xs:text-8xl">
 					{title.firstPart}{' '}
 					<span className="text-primary">{title.secondPart}</span>
 				</h1>
-				<p className="max-w-4xl text-center text-white sm:text-lg">
+				<p className="max-w-4xl text-center text-sm text-white sm:text-lg heroBiggerFont:text-base">
 					{description}
 				</p>
 				<Button icon={ArrowRight} moveIcon asChild>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -61,6 +61,7 @@ export default {
 			},
 			screens: {
 				xs: '475px',
+				heroBiggerFont: { raw: '(min-height: 700px)' },
 			},
 			textColor: {
 				'form-field-placeholder': '#727272',
