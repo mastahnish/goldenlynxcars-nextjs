873a54845d9196a95e53cd3562aa5115ea3d2153
Kacper Polak <strixu155@gmail.com>
2025-06-17 13:11:55 +0200

    fix: fix build

--- src/app/(payload)/admin/importMap.js
+++ src/app/(payload)/admin/importMap.js
@@ -1,15 +1,22 @@
-import { default as default_cc7a4f80ac23171466f2cd0838a1fa19 } from '@/payload/collections/dashboard/Rentals/ui/status-action-button/status-action-button'
-import { default as default_2a02436c9a34a0910db9fb9346d2f78c } from '@/payload/collections/dashboard/Rentals/ui/rental-state-field/rental-state-field'
-import { default as default_6cae7c906b0a21b0d2bd788a4245c343 } from '@/payload/collections/dashboard/Rentals/ui/add-signature-button/add-signature-button'
-import { default as default_f5cf4ebd37e2aaf6a24800869f9508fc } from '@/payload/components/dashboard/generate-contract-button/generate-contract-button'
-import { default as default_ace7e311b6d4a97d9cee4452ba752098 } from '@/payload/collections/dashboard/RentalsSchedule/ui/rentals-schedule/rentals-schedule'
-import { GcsClientUploadHandler as GcsClientUploadHandler_06e62ca02c7c441053a9b643e5545934 } from '@payloadcms/storage-gcs/client'
+import { GcsClientUploadHandler as GcsClientUploadHandler_06e62ca02c7c441053a9b643e5545934 } from '@payloadcms/storage-gcs/client';
+
+import { default as default_6cae7c906b0a21b0d2bd788a4245c343 } from '@/payload/collections/dashboard/Rentals/ui/add-signature-button/add-signature-button';
+import { default as default_2a02436c9a34a0910db9fb9346d2f78c } from '@/payload/collections/dashboard/Rentals/ui/rental-state-field/rental-state-field';
+import { default as default_cc7a4f80ac23171466f2cd0838a1fa19 } from '@/payload/collections/dashboard/Rentals/ui/status-action-button/status-action-button';
+import { default as default_ace7e311b6d4a97d9cee4452ba752098 } from '@/payload/collections/dashboard/RentalsSchedule/ui/rentals-schedule/rentals-schedule';
+import { default as default_f5cf4ebd37e2aaf6a24800869f9508fc } from '@/payload/components/dashboard/generate-contract-button/generate-contract-button';
 
 export const importMap = {
-  "@/payload/collections/dashboard/Rentals/ui/status-action-button/status-action-button#default": default_cc7a4f80ac23171466f2cd0838a1fa19,
-  "@/payload/collections/dashboard/Rentals/ui/rental-state-field/rental-state-field#default": default_2a02436c9a34a0910db9fb9346d2f78c,
-  "@/payload/collections/dashboard/Rentals/ui/add-signature-button/add-signature-button#default": default_6cae7c906b0a21b0d2bd788a4245c343,
-  "@/payload/components/dashboard/generate-contract-button/generate-contract-button#default": default_f5cf4ebd37e2aaf6a24800869f9508fc,
-  "@/payload/collections/dashboard/RentalsSchedule/ui/rentals-schedule/rentals-schedule#default": default_ace7e311b6d4a97d9cee4452ba752098,
-  "@payloadcms/storage-gcs/client#GcsClientUploadHandler": GcsClientUploadHandler_06e62ca02c7c441053a9b643e5545934
-}
+	'@/payload/collections/dashboard/Rentals/ui/status-action-button/status-action-button#default':
+		default_cc7a4f80ac23171466f2cd0838a1fa19,
+	'@/payload/collections/dashboard/Rentals/ui/rental-state-field/rental-state-field#default':
+		default_2a02436c9a34a0910db9fb9346d2f78c,
+	'@/payload/collections/dashboard/Rentals/ui/add-signature-button/add-signature-button#default':
+		default_6cae7c906b0a21b0d2bd788a4245c343,
+	'@/payload/components/dashboard/generate-contract-button/generate-contract-button#default':
+		default_f5cf4ebd37e2aaf6a24800869f9508fc,
+	'@/payload/collections/dashboard/RentalsSchedule/ui/rentals-schedule/rentals-schedule#default':
+		default_ace7e311b6d4a97d9cee4452ba752098,
+	'@payloadcms/storage-gcs/client#GcsClientUploadHandler':
+		GcsClientUploadHandler_06e62ca02c7c441053a9b643e5545934,
+};
